f837060779c8e343bf9480721bd7a83cc9420088
Diuzinxx <chiquinhoivo8@gmail.com>
2026-09-15 21:38:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -197,3 +197,4 @@ if __name__ == "__main__":
         from waitress import serve
         port = int(os.environ.get("PORT", 8000))
         print(f"Servindo em http://0.0.0.0:{port} — {SITE_NAME}")
+        serve(app, host="0.0.0.0", port=port, threads=8)
